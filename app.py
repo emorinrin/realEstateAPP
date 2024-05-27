0fb5708c4b8a49f6a8af8317389916eee0c80041
kishi_masahiro <ksmshr1988@gmail.com>
2024-05-27 22:08:48 +0900

    ブックマーク機能

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 import streamlit as st
 import pandas as pd
-import numpy as np
 import sqlite3
+import hashlib
 from geopy.geocoders import Nominatim
 import folium
 from streamlit_folium import folium_static
-import hashlib
 
 # データベース接続の設定
 DB_PATH = 'database.db'
@@ -33,6 +32,47 @@ def login_user(username, password):
     c.execute('SELECT * FROM userstable WHERE username =? AND password = ?', (username, password))
     data = c.fetchall()
     return data
+
+# ブックマークのデータベース設定
+def create_bookmark_table(username):
+    user_db = f"bookmark/{username}.db"
+    user_conn = sqlite3.connect(user_db)
+    user_c = user_conn.cursor()
+    user_c.execute('''CREATE TABLE IF NOT EXISTS bookmarks (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        名称 TEXT,
+                        アドレス TEXT,
+                        階数 TEXT,
+                        家賃 REAL,
+                        間取り TEXT,
+                        物件詳細URL TEXT,
+                        緯度 REAL,
+                        経度 REAL,
+                        区 TEXT
+                    )''')
+    user_conn.commit()
+    user_conn.close()
+
+def add_bookmark(username, bookmark):
+    user_db = f"bookmark/{username}.db"
+    user_conn = sqlite3.connect(user_db)
+    user_c = user_conn.cursor()
+    user_c.execute('''INSERT INTO bookmarks (名称, アドレス, 階数, 家賃, 間取り, 物件詳細URL, 緯度, 経度, 区)
+                      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+                   (bookmark['名称'], bookmark['アドレス'], bookmark['階数'], bookmark['家賃'], 
+                    bookmark['間取り'], bookmark['物件詳細URL'], bookmark['緯度'], 
+                    bookmark['経度'], bookmark['区']))
+    user_conn.commit()
+    user_conn.close()
+
+def load_bookmarks(username):
+    user_db = f"bookmark/{username}.db"
+    user_conn = sqlite3.connect(user_db)
+    user_c = user_conn.cursor()
+    user_c.execute('SELECT * FROM bookmarks')
+    bookmarks = user_c.fetchall()
+    user_conn.close()
+    return bookmarks
 
 # 賃貸物件データベースの設定
 RENT_DB_PATH = 'DB/room.db'
@@ -78,7 +118,7 @@ def toggle_show_all():
 def load_data_from_db(db_path):
     try:
         conn = sqlite3.connect(db_path)
-        query = f"SELECT * FROM {RENT_DB_TABLE_NAME}"
+        query = f"SELECT rowid as id, * FROM {RENT_DB_TABLE_NAME}"
         df = pd.read_sql(query, conn)
         conn.close()
         return df
@@ -118,14 +158,20 @@ def create_map(filtered_df):
 
     return m
 
-def display_search_results(filtered_df):
+def display_search_results(filtered_df, username):
     filtered_df['物件番号'] = range(1, len(filtered_df) + 1)
-    filtered_df['物件詳細URL'] = filtered_df['物件詳細URL'].apply(lambda x: make_clickable(x, "リンク"))
-    display_columns = ['物件番号', '名称', 'アドレス', '階数', '家賃', '間取り', '物件詳細URL']
-    filtered_df_display = filtered_df[display_columns]
-    st.markdown(filtered_df_display.to_html(escape=False, index=False), unsafe_allow_html=True)
 
-def rental_app():
+    # チェックボックスを追加
+    for idx, row in filtered_df.iterrows():
+        st.write(f"家賃：{row['名称']}")
+        st.write(f"家賃：{row['アドレス']}")
+        st.write(f"家賃：{row['家賃']}万円、階数{row['階数']}、間取り：{row['間取り']}")
+        st.image(row['物件画像URL'])
+        st.markdown("詳細リンク(%s)" % row['物件詳細URL'])
+        if st.checkbox(f"お気に入り登録する", key=f"{username}_{row['id']}"):
+            add_bookmark(username, row)
+
+def rental_app(username):
     df = load_data_from_db(RENT_DB_PATH)
     df = preprocess_dataframe(df)
 
@@ -182,14 +228,14 @@ def rental_app():
 
     if st.session_state.get('search_clicked', False):
         if st.session_state['show_all']:
-            display_search_results(st.session_state.get('filtered_df', filtered_df))
+            display_search_results(st.session_state.get('filtered_df', filtered_df), username)
         else:
-            display_search_results(st.session_state.get('filtered_df2', filtered_df2))
+            display_search_results(st.session_state.get('filtered_df2', filtered_df2), username)
 
 def main():
     st.title("賃貸物件情報アプリ")
 
-    menu = ["ホーム", "ログイン", "サインアップ"]
+    menu = ["ホーム", "ログイン", "サインアップ", "ブックマーク"]
     choice = st.sidebar.selectbox("メニュー", menu)
 
     if choice == "ホーム":
@@ -206,8 +252,9 @@ def main():
 
             result = login_user(username, check_hashes(password, hashed_pswd))
             if result:
-                st.success("{}さんでログインしました".format(username))
-                rental_app()
+                st.success(f"{username}さんでログインしました")
+                create_bookmark_table(username)
+                rental_app(username)
             else:
                 st.warning("ユーザー名かパスワードが間違っています")
 
@@ -221,6 +268,23 @@ def main():
             add_user(new_user, make_hashes(new_password))
             st.success("アカウントの作成に成功しました")
             st.info("ログイン画面からログインしてください")
+
+    elif choice == "ブックマーク":
+        st.subheader("ブックマークされた物件情報")
+        username = st.sidebar.text_input("ユーザー名を入力してください")
+        password = st.sidebar.text_input("パスワードを入力してください", type='password')
+        if st.sidebar.checkbox("表示"):
+            result = login_user(username, check_hashes(password, make_hashes(password)))
+            if result:
+                bookmarks = load_bookmarks(username)
+                if bookmarks:
+                    df_bookmarks = pd.DataFrame(bookmarks, columns=["id", "名称", "アドレス", "階数", "家賃", "間取り", "物件詳細URL",'緯度',
+                    '経度', '区'])
+                    st.dataframe(df_bookmarks)
+                else:
+                    st.warning("ブックマークされた物件がありません")
+            else:
+                st.warning("ユーザー名かパスワードが間違っています")
 
 if __name__ == '__main__':
     main()
